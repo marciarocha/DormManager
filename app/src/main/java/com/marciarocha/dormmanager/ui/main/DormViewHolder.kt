b30b4798d1cec6e137d4f6885b7b3f27eb48d350
marciarocha <marcia.rocha@lab-box.com>
2019-06-20 22:22:02 +0100

    add unversioned files

---
+++ app/src/main/java/com/marciarocha/dormmanager/ui/main/DormViewHolder.kt
@@ -0,0 +1,15 @@
+package com.marciarocha.dormmanager.ui.main
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.marciarocha.dormmanager.domain.model.Dorm
+import kotlinx.android.synthetic.main.dorm_item.view.*
+
+class DormViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    fun bind(dorm: Dorm, onSelect: (Dorm) -> Unit) {
+        itemView.dorm_description.text = dorm.description
+        itemView.price_per_bed.text = "$$dorm.price"
+        itemView.dorm_layout.setOnClickListener { onSelect(dorm) }
+    }
+}
