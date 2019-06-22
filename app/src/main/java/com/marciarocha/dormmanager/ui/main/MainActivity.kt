d5ec2561ebf1429ccfe4f34813468d3edb3e3fce
marciarocha <marcia.rocha@lab-box.com>
2019-06-22 21:58:25 +0100

    feat: main buttons are rounded and added cardview for information about total price

--- app/src/main/java/com/marciarocha/dormmanager/ui/main/MainActivity.kt
+++ app/src/main/java/com/marciarocha/dormmanager/ui/main/MainActivity.kt
@@ -47,11 +47,9 @@ class MainActivity : AppCompatActivity(), OnDialogResultListener {
             when (availableDormsState) {
                 is AvailableDormsState.Loading -> {
                     progress_wheel_dormslist.visibility = View.VISIBLE
-                    info_about_prices.visibility = View.GONE
                     error_message.visibility = View.GONE
                 }
                 is AvailableDormsState.Loaded -> {
-                    info_about_prices.visibility = View.VISIBLE
                     error_message.visibility = View.GONE
                     progress_wheel_dormslist.visibility = View.GONE
                     adapter.update(availableDormsState.dorms.toMutableList())
@@ -59,14 +57,14 @@ class MainActivity : AppCompatActivity(), OnDialogResultListener {
                 is AvailableDormsState.Error -> {
                     error_message.visibility = View.VISIBLE
                     progress_wheel_dormslist.visibility = View.GONE
-                    info_about_prices.visibility = View.GONE
                 }
             }
         })
 
         viewModel.totalCost.observe(this, Observer {
-            checkout_button.visibility = it.visibility
-            checkout_button.text = getString(R.string.checkout_button) + " ${it.price}"
+            total_cost_cardview.visibility = it.visibility
+            checkout_button.text = getString(R.string.checkout_button)
+            total_price_textView.text = "${it.price} " + getString(R.string.USD)
         })
     }
 
